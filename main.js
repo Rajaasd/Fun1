9b15a6bd2c6373fd621bf9aeec72bc182b330645
Rajaasd <147926160+Rajaasd@users.noreply.github.com>
2024-02-27 01:55:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//console.log('Hello World!');
+function remove(){
+	let name =  document.getElementById('clear1').value ="";
+	let mbno= document.getElementById('clear').value = "";
+
+	let pwd = document.getElementById('clear2').value ="";
+}
+	function check() {
+		let name =  document.getElementById('clear1').value ="";
+	let mbno= document.getElementById('clear').value = "";
+
+	let pwd = document.getElementById('clear2').value ="";
+		
+		if(name=="Hari"){
+			alert("Enter your details");
+		}
+		else if(mbno ==123){
+			al
+		}
+	//	window.open("main.html");
+	}
+
+
